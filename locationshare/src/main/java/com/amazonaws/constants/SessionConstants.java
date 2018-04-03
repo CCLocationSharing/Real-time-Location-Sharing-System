1d36928ed0ac7c6813b5fbbef19e8b42a7df1e20
lorraineM <31805613+lorraineM@users.noreply.github.com>
2018-04-03 12:07:47 -0400

    temp

--- locationshare/src/main/java/com/amazonaws/constants/SessionConstants.java
+++
@@ -1,8 +0,0 @@
-package com.amazonaws.constants;
-
-public interface SessionConstants {
-	static final String SESSION_SIGNIN_USER = "signInUser";
-	static final String SUCCESS = "s";
-	static final String SESSION_LAST_VISITED_URL = "lastVisitedUrl";
-	static final String SESSION_BY = "by";
-}
