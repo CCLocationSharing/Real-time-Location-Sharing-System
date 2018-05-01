4dac4efe4924b20b6ba0ffa243eb5bbc5b5abd5d
zhengyr <stella.zhengyr@gmail.com>
2018-05-01 18:25:58 -0400

    dummy data10

--- mldata/ml_data4.py
+++ mldata/ml_data4.py
@@ -3,5 +3,5 @@ from random import *
 with open('ccMlData.csv', 'w') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    for k in range(50):
+    for k in range(10000):
         filewriter.writerow(["\"YES\"", 1])
