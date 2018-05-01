d97dd7397eac203fe4b2ed276ce05b96e1e0c721
zhengyr <stella.zhengyr@gmail.com>
2018-05-01 17:56:49 -0400

    dummy data9

--- mldata/ml_data4.py
+++ mldata/ml_data4.py
@@ -4,4 +4,4 @@ with open('ccMlData.csv', 'w') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
     for k in range(50):
-        filewriter.writerow(["YES", 1])
+        filewriter.writerow(["\"YES\"", 1])
