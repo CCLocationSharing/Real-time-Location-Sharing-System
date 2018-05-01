8adcbda240ea75aff5a9d56efa8a80c7c63051ae
zhengyr <stella.zhengyr@gmail.com>
2018-05-01 13:21:12 -0400

    dummy data3

--- mldata/ml_data3.py
+++ mldata/ml_data3.py
@@ -4,15 +4,9 @@ with open('ccMlData.csv', 'w') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
     for k in range(100):
-        libId = randint(0, 1)
-        if(libId==1):
-            if(random()<0.9):
-                filewriter.writerow(["YES", 1])
-            else:
-                filewriter.writerow(["NO", 0])
-
-        else:
-            if(random()<0.9):
-                filewriter.writerow(["NO", 0])
-            else:
-                filewriter.writerow(["NO", 1])
+        varone = random()
+        vartwo = random()
+        varthree = random()
+        varfour = random()
+        result = 0.1 *varone + 0.2*vartwo+0.3*varthree+0.4*varfour
+        filewriter.writerow([result, varone, vartwo, varthree, varfour])
