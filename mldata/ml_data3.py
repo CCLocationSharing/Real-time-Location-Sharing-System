8395e9d809b489ba1bb838720699edc289d720aa
zhengyr <stella.zhengyr@gmail.com>
2018-05-01 13:04:08 -0400

    dummy data2

--- mldata/ml_data3.py
+++ mldata/ml_data3.py
@@ -7,14 +7,12 @@ with open('ccMlData.csv', 'w') as csvfile:
         libId = randint(0, 1)
         if(libId==1):
             if(random()<0.9):
-                filewriter.writerow([1, 1])
+                filewriter.writerow(["YES", 1])
             else:
-                filewriter.writerow([1, 0])
+                filewriter.writerow(["NO", 0])
 
         else:
             if(random()<0.9):
-                filewriter.writerow([0, 0])
+                filewriter.writerow(["NO", 0])
             else:
-                filewriter.writerow([0, 1])
-
-        
+                filewriter.writerow(["NO", 1])
