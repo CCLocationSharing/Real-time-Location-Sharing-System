badf87278e244d3eade1bf620eb308163fb92873
zhengyr <stella.zhengyr@gmail.com>
2018-05-01 17:50:07 -0400

    dummy data7

---
+++ mldata/ml_data4.py
@@ -0,0 +1,7 @@
+import csv
+from random import *
+with open('ccMlData.csv', 'w') as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for k in range(50):
+        filewriter.writerow([1, 1])
